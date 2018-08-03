466a3e36782301a13d6c2ff92e27fdd8f4f92498
Matthew M Dean <fireculex@gmail.com>
2018-08-02 23:22:14 -0700

    add balance and resto
    possibly incomplete

--- MaxDps_Druid/main.lua
+++ MaxDps_Druid/main.lua
@@ -5,7 +5,16 @@ end
 
 local Druid = MaxDps:NewModule('Druid');
 -- Spells
-		
+	local _Moonfire = 8921;
+	local _MoonfireTarget = 164812;
+	local _Wrath = 5176;
+	local _Sunfire = 93402;
+	local _SunfireTarget = 164815;
+	local _StarSurge = 78674;
+	local _BalanceWrath = 190984;
+	local _LunarStrike = 194153;
+	local _LunarEmpowerment = 164547;
+	
 -- General
 
 function Druid:Enable()
@@ -23,20 +32,56 @@ function Druid:Enable()
 	return true;
 end
 
-function Druid.Balance(timeShift, currentSpell, gcd, talents)
+function Druid:Balance(timeShift, currentSpell, gcd, talents)
+
+local astralPower = UnitPower('player', Enum.PowerType.LunarPower);
+
+	if not MaxDps:TargetAura(_MoonfireTarget, timeShift) then
+		return _Moonfire;
+	end
+	
+	if not MaxDps:TargetAura(_SunfireTarget, timeShift) then
+		return _Sunfire;
+	end
+
+	if astralPower >=80 and MaxDps:SpellAvailable(_StarSurge, timeShift) then
+		return _StarSurge;
+	end
+	
+	if MaxDps:Aura(_LunarEmpowerment, timeShift) then
+		return _LunarStrike;
+	end
+	
+	if MaxDps:SpellAvailable(_BalanceWrath, timeShift) then
+		return _BalanceWrath;
+	end
+	
+end
+
+function Druid:Feral(timeShift, currentSpell, gcd, talents)
 
 end
 
-function Druid.Feral(timeShift, currentSpell, gcd, talents)
+function Druid:Guardian(timeShift, currentSpell, gcd, talents)
 
 end
 
-function Druid.Guardian(timeShift, currentSpell, gcd, talents)
+function Druid:Restoration(timeShift, currentSpell, gcd, talents)
 
-end
+	if not MaxDps:TargetAura(_MoonfireTarget, timeShift) then
+		return _Moonfire;
+	end
+	
+	if not MaxDps:TargetAura(_SunfireTarget, timeShift) then
+		return _Sunfire;
+	end
 
-function Druid.Restoration(timeShift, currentSpell, gcd, talents)
 
+	if MaxDps:SpellAvailable(_Wrath, timeShift) then
+		return _Wrath;
+	end
+	
+	
 end
 
 
